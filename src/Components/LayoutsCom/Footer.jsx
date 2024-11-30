b9048c068fef9d275439de6a1d51b11142b94815
Alejandro Vargas <alejovastillo@gmail.com>
2024-11-29 20:33:13 -0500

    React Router Dom work basic

---
+++ src/Components/LayoutsCom/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <>
+      <div className='border-2 border-blue-500 border-solid'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum rerum quae possimus doloribus labore laboriosam recusandae, ratione et facilis unde ad iste exercitationem beatae aliquid, porro, mollitia non natus enim!
+      </div>
+        
+    </>
+  );
+};
+export default Footer
