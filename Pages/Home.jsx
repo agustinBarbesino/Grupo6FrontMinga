ef564ac0a054cdcba45f5363cd7a39a987a1f064
Alejandro Vargas <alejovastillo@gmail.com>
2024-11-29 20:38:07 -0500

    Delete Home.jsx

--- Pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-
-    return (
-        <>
-            <div className="border border-red-500 border-solid">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia fuga, amet sequi laborum eius voluptas explicabo necessitatibus, suscipit illum deserunt ipsam voluptatibus iusto, eos nisi tempora ut commodi. Expedita, odio!</div>
-        </>
-    );
-}
