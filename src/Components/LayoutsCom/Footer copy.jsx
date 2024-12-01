cb27146933fa7e2c0e6b2948cf45010396a6c0f4
Samantha Hernandez <samhercab@gmail.com>
2024-11-30 20:16:34 -0600

    primer coomit, footer completo

---
+++ src/Components/LayoutsCom/Footer copy.jsx
@@ -0,0 +1,15 @@
+import './footer.css';
+const Footer = () => {
+
+    return (
+        <>
+            <div className='relative w-full min-h-[30vh] overflow-hidden bg-footer border-2 border-red-500'>
+                <img src="https://s3-alpha-sig.figma.com/img/fcce/6712/353a5270e91eeb0d409a11fa6f598267?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IwS5A6iz-YJmkm7H1o8Xxl8AWB5CCGojk2a1w6D6obNW4z5Y71XKDBo~o6rI8y9jZBtxYVKVGQlkxAh842gqwEL~00d1QSo5mH9uI87IWaxQIIEdVjWd2-zyoNVzKBThlmG~OR3Sj-v9xUM17a2DLbiLktMORW4x3S1FAwdEcUjePpb4rbyevYrzWRy3EnalgW5hBq4vrRuIW4gDC34q2n4jKVjpjTlYz2NMgjvjHh0XvFzZQUluykS2e6PRWA1FdClvfWHOJQ9IPn-LStxUddI29glRnHmAHBS~4NYUXYDsp3W-PyaM-M~JeDGpLUeT~rcl1sfBDPvdm5rJXlXF~Q__" alt="recorte"
+                    className="absolute w-full h-full object-cover clip-ellipse-custom" />
+            </div>
+
+        </>
+    );
+};
+
+export default Footer
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ export default {
   theme: {
     extend: {
       clipPath: {
-        'ellipse-custom': 'ellipse(20% 20% at 20% 5%)',
+        'footer': 'ellipse(60% 80% at 50% 20%)',
+      },
+      backgroundImage: {
+        'footer': "url('/src/assets/bg.jpg')",    
+        'pink-gradient': 'linear-gradient(to right, #F9A8D4, #F472B6)',    
+      },
+      colors: {
+        'rose-light': '#F9A8D4',
+        'rose-dark': '#F472B6',
+      },
+      fontFamily:{
+        'montserrat': ['Montserrat'],
+   
       },
     },
   },
