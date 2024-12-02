a3b08200db8087604798d74a261c89ac0404f1a6
Samantha Hernandez <samhercab@gmail.com>
2024-12-02 12:22:10 -0600

    ocultar footer para movil, terminar navbar

--- src/Components/LayoutsCom/Header.jsx
+++ src/Components/LayoutsCom/Header.jsx
@@ -1,12 +1,22 @@
 
 
+import NavBar from "./Navbar"
+import { NavLink } from "react-router-dom"
+import img from '../../assets/logoMinga.png'
+
 const Header = () => {
-
-
   return (
-    <div className='border-2 border-red-500 border-solid '>
-     
-    </div>
-  );
+    <>
+      <header className="relative w-full bg-transparent z-50">
+        <div className="flex fixed w-full justify-between font-montserrat px-2 md:px-6 py-4">
+          <NavBar/>
+          {/* logo */}
+          <NavLink to="/Home" className="flex justify-center md:justify-end 1/2 md:w-1/4">  <img src={img} className="h-auto w-full md:w-20" alt="LogoManga" />
+          </NavLink>
+        </div>
+
+      </header>
+    </>
+  )
 }
-export default Header;
+export default Header
