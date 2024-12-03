5fce5e3de5a3f130c8f22aa919da536c1971b394
Alejandro Vargas <alejovastillo@gmail.com>
2024-12-03 18:29:15 -0500

    fix home error react

--- src/Pages/Chapter.jsx
+++
@@ -1,7 +0,0 @@
-import ChapterPages from '../Components/ChapterPages/ChapterPages';
-export default function Chapter() {
-
-    return (
-        <ChapterPages/>
-    );
-}
--- src/Pages/PanelAdmin.jsx
+++
@@ -1,9 +0,0 @@
-import Panel from "../Components/Panel/PanelComponent";
-
-export default function PanelPage() {
-  return (
-    <div className="min-h-screen bg-white pt-20 pb-40 relative">
-      <Panel />
-    </div>
-  );
-}
