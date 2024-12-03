fcc4bef161cceff3cc6cd90abc8a5e189d7c5e8c
Alejandro Vargas <alejovastillo@gmail.com>
2024-12-03 18:33:27 -0500

    fixed home app

---
+++ src/Pages/Favorites.jsx
@@ -0,0 +1,9 @@
+import Favorites from "../Components/Favorites/FavoritesE";
+
+export default function FavoritesPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 pt-20">
+      <Favorites />
+    </div>
+  );
+}
