52c08e7e4d81ff20828f7bf011fd461ec55295f0
Carlos Rene Perez G <d.londocreed@gmail.com>
2024-12-03 12:23:55 -0600

    Panel Add

---
+++ src/Pages/PanelAdmin.jsx
@@ -0,0 +1,9 @@
+import Panel from "../Components/Panel/PanelComponent";
+
+export default function PanelPage() {
+  return (
+    <div className="min-h-screen bg-white pt-20 pb-40 relative">
+      <Panel />
+    </div>
+  );
+}
