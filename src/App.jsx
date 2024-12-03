91a5130f8a89701e58f469d8b21f3dacbd3dfa2f
Alejandro Vargas <alejovastillo@gmail.com>
2024-12-02 20:05:30 -0500

    Use swiper, cause read a chapter

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import StandarLayout from "./Layouts/StandarLayout";
 
 //Pages
 import Home from "./Pages/Home";
+import Chapter from "./Pages/Chapter";
 import NotFound from "./Pages/NotFound";
 import Profile from './Components/Profile/ProfileComponent'
 
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
       { path: "/", element: <Home /> },
       { path: "/home", element: <Home /> },
       { path: "/profile", element: <Profile /> },
+      { path: "/chapter", element: <Chapter /> },
     ],
   },
   
---
+++ src/Components/ChapterPages/ChapterPages.jsx
@@ -0,0 +1,72 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules';
+
+//css
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css';
+import './ChapterPages.css'
+
+//data
+import { mangas } from '../../MockData/dataChapters'
+console.log(mangas[0].chapters[0].pages);
+
+
+export default function ChapterPages() {
+
+    return (
+        <>
+            <Swiper className='mySwiper'
+                slidesPerView={1}
+                slidesPerGroup={1}
+                spaceBetween={30}
+                freeMode={true}
+                navigation={true}
+                modules={[Navigation, Pagination]}
+                effect='fade'
+                fadeEffect={{
+                    crossFade: true,
+                }}
+                speed={2000}
+                breakpoints={{
+                    480: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                        slidesPerGroup: 4,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 40,
+                        slidesPerGroup: 3,
+                    },
+                    1024: {
+                        slidesPerView: 1,
+                        spaceBetween: 50,
+                        slidesPerGroup: 1,
+                    },
+                }}
+
+            >
+
+
+                {mangas[0].chapters[0].pages.map((p) => (
+                    <SwiperSlide >
+                        {/* <div className='mb-6 relative shadow-md rounded-xl overflow-hidden '>
+                <div className='shadow-text h-56 rounded-xl  flex justify-center items-center '>
+                <img
+                    src={d.img}
+                    alt={d.name}
+                    className='h-44 w-44 object-cover'
+                />
+                </div>
+                <div className='p-4 absolute bottom-3 right-3 z-20'>
+                <p className='text-white font-bold text-3xl'>{d.name}</p>
+                </div>
+            </div> */}
+                        <img src={p} alt=""  className=''/>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </>
+    );
+}
