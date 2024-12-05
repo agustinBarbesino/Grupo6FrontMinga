ddce0ad01ab2601ea6a6c8afeeed2aa0fbe5fd77
jhoselerodriguez <jhoselerodriguez@gmail.com>
2024-12-04 21:28:21 -0500

    guardado store

--- src/store/store.js
+++ src/store/store.js
@@ -1,34 +1,21 @@
-<<<<<<< HEAD
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "./reducer/authReducer";
 import categoryReducer from "./reducer/categoryReducer";
 import mangasReducer from "./reducer/mangasReducer";
 import  mangasFilterReducer from "./mangaSlice";
-import authReducer from "./reducer/authReducer";
+
 
 
 
 const store = configureStore({
     reducer: {
       auth:authReducer,
+      categories: categoryReducer,
         mangasFilterStore:mangasFilterReducer,
-        categoryStore:categoryReducer,
         mangasStore:mangasReducer
     },
 });
 
 
-=======
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './reducer/authReducer'
-import categoryReducer from './reducer/categoryReducer'
-
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    categories: categoryReducer
-  },
-})
->>>>>>> 1c27e5e4de8c6be5eb943289070bc9acb0c7f647
 
 export default store
