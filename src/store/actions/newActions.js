8802c0f7a7e6a9b1713b6a6783614de65dcd5ceb
Samantha Hernandez <samhercab@gmail.com>
2024-12-06 13:39:34 -0600

    create Manga full conectado

--- src/store/actions/newActions.js
+++ src/store/actions/newActions.js
@@ -2,8 +2,9 @@ import { createAction, createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 const BASE_URL = 'http://localhost:8080/api'
 
-export const createManga = createAsyncThunk('createManga', async({updatedFormData}) => {
-    const response = await axios.post(`${BASE_URL}/mangas/create`, updatedFormData)
+export const createManga = createAsyncThunk('createManga', async({formData}) => {
+    console.log("updatedFormBack", formData)
+    const response = await axios.post(`${BASE_URL}/mangas/create`, formData)
     return response.data
 }
 )
